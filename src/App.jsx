0429e3fce220868956d5610c8d70313e9a7898f4
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-06-06 23:43:33 +0330

    config styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <h2>hello world , MJR </h2>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The wild oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
     </>
   );
 }
