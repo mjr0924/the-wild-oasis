17dd2fd70b31571a2d9b93bb3010e185075fdddc
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-06-07 19:03:59 +0330

    add & setting up supabase(create db & add Table & config base of supabase)

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://frgawdtuivumxwjzaood.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyZ2F3ZHR1aXZ1bXh3anphb29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyODM2NTYsImV4cCI6MjA2NDg1OTY1Nn0.mblwMxuiMWp78Zl-DGtiYt9CuuoCkrHqwwUkVEmySAw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
