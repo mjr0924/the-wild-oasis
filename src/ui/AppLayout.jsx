e3dbba0b830b2e08d70d951c09659a8169f96f4b
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-06-21 22:02:15 +0330

    Toast Notification added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,6 +13,13 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
 
 function AppLayout() {
   return (
@@ -20,7 +27,9 @@ function AppLayout() {
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
