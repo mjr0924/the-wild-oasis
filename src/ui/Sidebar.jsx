92439bb34eed5019f63fdcac1c9eee6036e11982
mohammad javad Rezaei <mjr0924@gmail.com>
2025-07-21 15:11:35 +0330

    Uploading Sample Data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -13,10 +14,14 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
-  return <StyledSidebar>
-    <Logo />
-    <MainNav />
-  </StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+
+      <Uploader />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
