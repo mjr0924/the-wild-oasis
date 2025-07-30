4fd61cbdc3d227c497b5035b853aa11cda18dd4b
mohammad javad Rezaei <mjr0924@gmail.com>
2025-07-30 21:44:09 +0330

    Computing Recent Bookings and Stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,13 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isPending: isPending1 } = useRecentBookings();
+  const { stays, confirmedStays, isPending: isPending2 } = useRecentStays();
+
+  if (isPending1 || isPending2) return <Spinner />;
+
+  console.log(bookings);
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
